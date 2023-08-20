a806df80e99f78fd2d27473e8204664b0f03da88
Nurujjaman329 <mdnurujjaman329@gmail.com>
2023-08-21 01:33:16 +0600

    Complete

--- infoapp/lib/homepage.dart
+++ infoapp/lib/homepage.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,12 +11,50 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<dynamic> users = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Api Call"),
+        elevation: 0,
+        title: Center(
+          child: Text("Rest Api Call"),
+        ),
       ),
+      body: ListView.builder(
+          itemCount: users.length,
+          itemBuilder: (context, index) {
+            final user = users[index];
+            final email = user['email'];
+            final picture = user['picture']['thumbnail'];
+            final name = user['name']['first'];
+            return ListTile(
+              leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(20.0),
+                  child: Image.network(picture)),
+              title: Text(email),
+              subtitle: Text(
+                name,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, color: Colors.blueGrey),
+              ),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.upload_file_rounded), onPressed: fetchUsers),
     );
+  }
+
+  void fetchUsers() async {
+    print('Fetch Users Called');
+    const url = 'https://randomuser.me/api/?results=100';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final body = response.body;
+    final json = jsonDecode(body);
+    setState(() {
+      users = json['results'];
+    });
+    print('Fetch Completed');
   }
 }
